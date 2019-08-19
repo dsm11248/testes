883ce7c3e337adda4fb517719b3235492ae94ac7
Pedro Felipe <pedrofelipest@gmail.com>
2019-08-19 16:14:43 -0300

    Testes React

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+//Importar o React
+import React from 'react';
+
+//Importar as dependencias do 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+//Importar os componentes da aplicação
+
+const Routes = () => {
+    <BrowserRouter>
+        <Route path='' component='' />
+    </BrowserRouter>
+}
+
+export default Routes;
